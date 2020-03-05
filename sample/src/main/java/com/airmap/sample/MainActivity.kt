e058a2ba1b018a799307c7d5840000c0c7abc476
Vansh Gandhi <vansh.gandhi@gmail.com>
2020-03-04 21:17:42 -0800

    Improved structure for testing

--- sample/src/main/java/com/airmap/sample/MainActivity.kt
+++ sample/src/main/java/com/airmap/sample/MainActivity.kt
@@ -7,11 +7,14 @@ import com.airmap.airmapsdk.AirMap
 import com.airmap.airmapsdk.AirMap.client
 import com.airmap.airmapsdk.Response
 import com.airmap.airmapsdk.models.Config
+import com.aungkyawpaing.geoshi.model.Polygon
+import com.aungkyawpaing.geoshi.model.Position
 import com.readystatesoftware.chuck.ChuckInterceptor
 import com.squareup.moshi.Moshi
 import okhttp3.OkHttpClient
 import timber.log.Timber
 import java.io.Reader
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,73 +22,90 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val okHttpClientBuilder = OkHttpClient.Builder().addInterceptor(ChuckInterceptor(this))
-        AirMap.init(getConfig(this, Moshi.Builder().build()), okHttpClientBuilder = okHttpClientBuilder)
-        demoAll()
+        AirMap.init(getConfig(this, Moshi.Builder().build()), false, okHttpClientBuilder)
+        try {
+            inProgress()
+        } catch (e: Exception) {
+            Timber.e(e)
+        }
+    }
+
+    private fun inProgress() {
+        client.getFlight().executeAndLogResponse()
+    }
+
+    private fun queue() {
+//            client.createFlight().executeAndLogResponse()// TODO: Test for Point, Path, and Polygon
+//            client.createFlightPoint().executeAndLogResponse()
+//            client.createFlightPath().executeAndLogResponse()
+//            client.createFlightPolygon().executeAndLogResponse()
+//            client.endFlight().executeAndLogResponse()
+//            client.deleteFlight().executeAndLogResponse()
+//            client.startComm().executeAndLogResponse()
+//            client.endComm().executeAndLogResponse()
+//            client.getFlightPlan().executeAndLogResponse()
+//            client.createFlightPlan().executeAndLogResponse()
+//            client.updateFlightPlan().executeAndLogResponse()
+//            client.submitFlightPlan().executeAndLogResponse()
+//            client.getFlightBriefing().executeAndLogResponse()
+//            client.updatePilot().executeAndLogResponse()
+//            client.getAircraft().executeAndLogResponse()
+//            client.createAircraft().executeAndLogResponse()
+//            client.updateAircraft().executeAndLogResponse()
+//            client.deleteAircraft().executeAndLogResponse()
+//            client.getRuleset().executeAndLogResponse()
+//            client.getRulesets().executeAndLogResponse()
+//            client.getRulesets().executeAndLogResponse()
+//            client.getRulesets().executeAndLogResponse()
+//            client.getEvaluation().executeAndLogResponse()
+//            client.getJurisdictions().executeAndLogResponse()
+//            client.getJurisdictions().executeAndLogResponse()
+//            client.getAdvisories().executeAndLogResponse()
+//            client.getAdvisories().executeAndLogResponse()
+//            client.getAdvisories().executeAndLogResponse()
+//            client.getAdvisories().executeAndLogResponse()
+    }
+
+    private fun notWorking() {
+        val rulesets = "usa_ama,usa_national_park,usa_sec_91,usa_wilderness_area,usa_sec_336".split(',')
+        val geometry = Polygon(
+            listOf(
+                listOf(
+                    Position(-122.1008657255241, 37.44169541637456),
+                    Position(-122.06715767436634, 37.44169541637456),
+                    Position(-122.06715767436634, 37.40233316192527),
+                    Position(-122.1008657255241, 37.40233316192527),
+                    Position(-122.1008657255241, 37.44169541637456)
+                )
+            )
+        )
+        client.getAdvisories(rulesets, geometry, Date(), Date()).executeAndLogResponse()
+        client.getAirspaces(listOf("97f107b9-5688-4842-b98c-18addfcb3a1f", "97f107b9-5688-4842-b98c-18addfcb3a1g")).executeAndLogResponse()
+    }
+
+    private fun working() {
+        client.getManufacturers().executeAndLogResponse()
+        client.getManufacturers("GoPro").executeAndLogResponse()
+        client.getModel("c7ed05c7-cbe1-43a4-b2a8-500d5607e994").executeAndLogResponse()
+        client.getModels().executeAndLogResponse()
+        client.getModels(manufacturerId = "63280fbf-3c7f-47f4-9168-5763899048cd").executeAndLogResponse()
+        client.getModels(name = "Karma").executeAndLogResponse()
+        client.getFlights(pilotId = "auth0|5761a4279732f5844b1db844").executeAndLogResponse()
+        client.getFlights().executeAndLogResponse()
+        client.getPilot().executeAndLogResponse()
+        client.sendVerificationToken().executeAndLogResponse()
+        client.verifySMS(187549).executeAndLogResponse()
+        client.getAllAircraft().executeAndLogResponse()
+        client.getAirspace("97f107b9-5688-4842-b98c-18addfcb3a1f").executeAndLogResponse()
     }
 
     private fun <T> genericLogResponseHandler(response: T?, error: Throwable?) {
         response?.let { Timber.v(it.toString()) }
         error?.let { Timber.e(it) }
     }
-    
+
     private fun <T> Response<T>.executeAndLogResponse() {
         execute(::genericLogResponseHandler)
-    }
-
-    private fun demoAll() {
-        try {
-//            client.getManufacturers().executeAndLogResponse()
-//            client.getManufacturers().executeAndLogResponse()
-//            client.getManufacturers("GoPro").executeAndLogResponse()
-//            client.getModel("c7ed05c7-cbe1-43a4-b2a8-500d5607e994").executeAndLogResponse()
-//            client.getModels().executeAndLogResponse()
-//            client.getModels(manufacturerId = "63280fbf-3c7f-47f4-9168-5763899048cd").executeAndLogResponse()
-//            client.getModels(name = "Karma").executeAndLogResponse()
-            client.getFlights(pilotId = "auth0|5761a4279732f5844b1db844").executeAndLogResponse()
-//            client.getFlights().executeAndLogResponse()
-//            client.getPilot().executeAndLogResponse()
-//            client.sendVerificationToken().executeAndLogResponse()
-//            client.getAllAircraft().executeAndLogResponse()
-        } catch (e: Exception) {
-            Timber.e(e)
-        }
-
-
-//        AirMap.client.getAdvisories().executeAndLogResponse()
-//        AirMap.client.getAirspace().executeAndLogResponse()
-//        AirMap.client.getAirspaces().executeAndLogResponse()
-//        AirMap.client.getFlight().executeAndLogResponse()
-//        AirMap.client.createFlight() .executeAndLogResponse()// TODO: Test for Point, Path, and Polygon
-//        AirMap.client.createFlightPoint().executeAndLogResponse()
-//        AirMap.client.createFlightPath().executeAndLogResponse()
-//        AirMap.client.createFlightPolygon().executeAndLogResponse()
-//        AirMap.client.endFlight().executeAndLogResponse()
-//        AirMap.client.deleteFlight().executeAndLogResponse()
-//        AirMap.client.startComm().executeAndLogResponse()
-//        AirMap.client.endComm().executeAndLogResponse()
-//        AirMap.client.getFlightPlan().executeAndLogResponse()
-//        AirMap.client.createFlightPlan().executeAndLogResponse()
-//        AirMap.client.updateFlightPlan().executeAndLogResponse()
-//        AirMap.client.submitFlightPlan().executeAndLogResponse()
-//        AirMap.client.getFlightBriefing().executeAndLogResponse()
-//        AirMap.client.updatePilot().executeAndLogResponse()
-//        AirMap.client.verifySMS().executeAndLogResponse()
-//        AirMap.client.verifySMS().executeAndLogResponse()
-//        AirMap.client.getAircraft().executeAndLogResponse()
-//        AirMap.client.createAircraft().executeAndLogResponse()
-//        AirMap.client.updateAircraft().executeAndLogResponse()
-//        AirMap.client.deleteAircraft().executeAndLogResponse()
-//        AirMap.client.getRuleset().executeAndLogResponse()
-//        AirMap.client.getRulesets().executeAndLogResponse()
-//        AirMap.client.getRulesets().executeAndLogResponse()
-//        AirMap.client.getRulesets().executeAndLogResponse()
-//        AirMap.client.getEvaluation().executeAndLogResponse()
-//        AirMap.client.getJurisdictions().executeAndLogResponse()
-//        AirMap.client.getJurisdictions().executeAndLogResponse()
-//        AirMap.client.getAdvisories().executeAndLogResponse()
-//        AirMap.client.getAdvisories().executeAndLogResponse()
-//        AirMap.client.getAdvisories().executeAndLogResponse()
-//        AirMap.client.getAdvisories().executeAndLogResponse()
     }
 
     private fun getConfig(context: Context, moshi: Moshi) = try {
